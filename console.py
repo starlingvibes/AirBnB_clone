488dc4d83fe57fb9729b61a9204538773096e8c8
starlingvibes <starlingroot@protonmail.com>
2022-09-02 14:19:17 +0100

    worked on all, destory and show commands

--- console.py
+++ console.py
@@ -2,7 +2,7 @@
 """ A module for the entry point of the command interpreter """
 import cmd
 import sys
-# from models import storage
+from models import storage
 from models.base_model import BaseModel
 
 
@@ -26,12 +26,93 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
+        """ Creates and saves a new instance of BaseModel to JSON file"""
         if not args:
             print("** class name missing **")
             return
         elif args not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
+        new_instance = HBNBCommand.classes[args]()
+        storage.save()
+        print(new_instance.id)
+        storage.save()
+
+    def do_show(self, args):
+        """ Prints the string representation of an instance based on class name """
+        # Splitting the multiple arguments
+        new = args.partition(" ")
+        c_name = new[0]
+        c_id = new[2]
+
+        # handling extra spaces
+        if c_id and ' ' in c_id:
+            c_id = c_id.partition(' ')[0]
+
+        if not c_name:
+            print("** class name missing **")
+            return
+
+        if c_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        if not c_id:
+            print("** instance id missing **")
+            return
+
+        key = c_name + "." + c_id
+        try:
+            print(storage._FileStorage__objects[key])
+        except KeyError:
+            print("** no instance found **")
+
+    def do_destroy(self, args):
+        """ Deletes an instance based on class name and id, writing the change into a JSON file """
+        new = args.partition(" ")
+        c_name = new[0]
+        c_id = new[2]
+
+        if c_id and ' ' in c_id:
+            c_id = c_id.partition(" ")[0]
+
+        if not c_name:
+            print("** class name missing **")
+            return
+
+        if c_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        if not c_id:
+            print("** instance id missing **")
+            return
+
+        key = c_name + "." + c_id
+        try:
+            del(storage.all()[key])
+            storage.save()
+        except KeyError:
+            print("** no instance found **")
+
+    def do_all(self, args):
+        """ Prints the string representation of all instances based or not on the class name """
+        print_list = []
+
+        if args:
+            # remove trailing arguments
+            args = args.split(" ")[0]
+            if args not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+                return
+            for key, value in storage._FileStorage__objects.items():
+                if k.split(".")[0] == args:
+                    print_list.append(str(value))
+        else:
+            for key, value in storage._FileStorage__objects.items():
+                print_list.append(str(value))
+
+        print(print_list)
 
 
 if __name__ == "__main__":
