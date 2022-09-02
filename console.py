b2c29afd814f2973f0ab9db524d973a9a01a104a
starlingvibes <starlingroot@protonmail.com>
2022-09-02 12:37:26 +0100

    boilerplate for console api

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" A module for the entry point of the command interpreter """
+import cmd
+import sys
+# from models import storage
+# from models.base_model import BaseModel
+
+
+class HBNBCommand(cmd.Cmd):
+    """ The command interpreter class inheriting from the `Cmd` class """
+    prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
+
+    def do_quit(self, command):
+        """ Method to exit the console """
+        exit()
+
+    def do_EOF(self, command):
+        """ Method to handle EOF """
+        print()
+        exit()
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
