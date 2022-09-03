281beb5dfcf7e961cce180d379fb0c4868e99399
starlingvibes <starlingroot@protonmail.com>
2022-09-03 14:35:05 +0100

    fixed error that occurs when creating instances

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
-            storage.new()
+            storage.new(self)
         else:
             kwargs['created_at'] = datetime.datetime.strptime(
                 kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
